32b1c3d9a695b0efb527114cfec71bfa7239019c
Luis Eduardo Abreu <luisciatteabreu@gmail.com>
2026-08-12 18:53:11 -0300

    T-05 — Alterado versão do Java para rodar testes e alterado classe ServicoCloudDef para record

--- backend/src/main/java/modelo/ServicoCloudDef.java
+++ backend/src/main/java/modelo/ServicoCloudDef.java
@@ -4,9 +4,9 @@ import java.util.Map;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class ServicoCloudDef {
- @NotBlank String id; // identificador do serviço ("API, DATA, STORAGE, COMPUTE, etc")
- @NotBlank String tipo; // tipo do serviço ("AWS, GCP, AZURE, etc")
- String categoria; // categoria do serviço  ("API, DATA, STORAGE, COMPUTE, etc") - Pode ser null
- Map<String, String> configuracao; // configurações do serviço (ex: "region", "accessKey", "secretKey", etc) - Pode ser null
-}
+public record ServicoCloudDef(
+    @NotBlank String id, // identificador do serviço ("API, DATA, STORAGE, COMPUTE, etc")
+    @NotBlank String tipo, // tipo do serviço ("AWS, GCP, AZURE, etc")
+    String categoria, // categoria do serviço  ("API, DATA, STORAGE, COMPUTE, etc") - Pode ser null
+    Map<String, String> configuracao // configurações do serviço (ex: "region", "accessKey", "secretKey", etc) - Pode ser null
+) {}
