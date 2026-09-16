ee81777363219676d8fc5bda33c482d760c500c8
GustavoLiborioEstevao <gustavoliborioestevao@gmail.com>
2026-09-15 21:12:17 -0300

    T-28 — ProjetoControlador: endpoints CRUD

--- backend/src/main/java/com/web2/DiagramCodeBackendApplication.java
+++ backend/src/main/java/com/web2/DiagramCodeBackendApplication.java
@@ -1,8 +1,18 @@
 package com.web2;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.web2",
+        "dao",
+        "servico",
+        "gerador"
+})
 public class DiagramCodeBackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DiagramCodeBackendApplication.class, args);
     }
